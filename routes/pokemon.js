181caac271835a85b2741bd95a45f6dbb27d947d
pbarbarone <peter.barbarone@gmail.com>
2017-12-28 20:54:43 -0800

    trying to route to individual pokemon page

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -7,13 +7,34 @@ var router = express.Router();
 // GET - return a page with favorited Pokemon
 router.get('/', function(req, res) {
     // TODO: render favorites
-    res.send('Render a page of favorites here');
+    db.pokemon.findAll().then(function(pokemons){
+    	res.render('favorites',{pokemons: pokemons});
+    });
 });
 
 // POST - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
     // TODO: add to database
+    db.pokemon.create(req.body).then(function(pokemon){
+    	res.redirect('/favorites');
+    })
     res.send(req.body);
 });
+
+// GET - go to individual pokemon page
+// todo: add sprite, and maybe more info
+router.get('/:id', function(req, res){
+	db.pokemon.findById(req.body.name).then(function(pokemon){
+		res.render('individual',{pokemon: pokemon})
+	});
+});
+
+// DELETE 
+// router.delete('/:name', function(req, res) {
+//   db.pokemon.findById(req.body.name).then(function(pokemon) {
+//     pokemon.destroy();
+//   });
+// });
+
 
 module.exports = router;
